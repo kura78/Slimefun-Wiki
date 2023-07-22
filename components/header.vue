8293169fbc73a26d4024471ec531ae01f1c82da6
kura78 <123511632+kura78@users.noreply.github.com>
2023-07-22 19:15:43 +0100

    Changed the Header Classes to topnav-left

--- components/header.vue
+++ components/header.vue
@@ -2,11 +2,11 @@
   <div> </div>
     <div class="headerContent sticky">
       <div class="wordmark">
-        <a class="headerLink" href="#">Slimefun Handbook      </a>
+        <a class="topnav-left" href="#">Slimefun Handbook      </a>
       </div>
       <div class="headerLinksArea">
-        <a class="headerLink" href="#"> Addons   </a>
-        <a class="headerLink" href="#"> Slimefun   </a>
+        <a class="topnav-left" href="#"> Addons   </a>
+        <a class="topnav-left" href="#"> Slimefun   </a>
         <a class="topnav-right" href="#"> Discord</a>
     </div>
       
