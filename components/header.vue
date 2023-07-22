6e5ee0b60a85a12ecc4bee69ff88c85c49f4541d
kura78 <123511632+kura78@users.noreply.github.com>
2023-07-22 18:46:47 +0100

    CSS Changes + Top bar
    Will most likely change again

---
+++ components/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="headerContent sticky">
+      <div class="wordmark">
+        <a class="headerLink" href="#">Slimefun Handbook      </a>
+      </div>
+      <div class="headerLinksArea">
+        <a class="headerLink" href="#"> Addons   </a>
+        <a class="headerLink" href="#"> Slimefun   </a>
+        <a class="headerLink" href="https://discord.gg/A5GgDn56n6"> Discord   </a>
+    </div>
+    </div>
+  </div>
+</template>
