c9c9e82454bfe66c4103021a3f0a8862cfdcc6f5
blueb <harperssademailaddress@gmail.com>
2023-07-22 16:16:35 -0400

    fix themes

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
       <div class="footerContent">
         <div class="footerTop">
           Made by Orchid Development &bull; This website is <a class="footerLink"
-            href="https://github.com/orchidtowny/bluejay" target="_blank" rel="noopener noreferrer">open source</a> on
+            href="https://github.com/orchidtowny/slimefun-wiki" target="_blank" rel="noopener noreferrer">open source</a> on
           Github.
         </div>
       </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,9 +7,6 @@ export default defineNuxtConfig({
       ],
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      bodyAttrs: {
-        'data-theme': 'light'
-      }
     },
   },
   pages: true,
