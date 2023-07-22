55127762ec568a57e810f520fe7a1c9e75ebabcb
kura78 <123511632+kura78@users.noreply.github.com>
2023-07-22 22:01:47 +0100

    Changed Cards.vue

--- components/Card.vue
+++ components/Card.vue
@@ -1,3 +1,31 @@
+<script>
+export default {
+    props: {
+        img: String,
+        title: String,
+    }
+}
+</script>
+
 <template>
-    
-</template>
+  <div class="card">
+    <div class="card-header">
+      <h3>{{ title }}</h3>
+    </div>
+    <div class="card-body">
+        <slot></slot>
+    </div>
+  </div>
+  </template>
+ 
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 40%;
+  border-radius: 5px;
+  align: center;
+}
+
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <script>
 import Header from '../components/Header.vue';
 import Footer from '../components/Footer.vue';
+import Card from '../components/Card';
+
 </script>
 <template>
     <Header />
